1d80caeab04e60049c3b21ba586c5904b57d94e4
rngKomorebi <brucebatmanwayne84@gmail.com>
2022-04-21 10:06:07 +0200

    Update Timestamp.py

--- Development/Timestamp.py
+++ Development/Timestamp.py
@@ -10,10 +10,12 @@ Data_matrix = np.zeros((256, int(len(Data)/256))) # rows=#pixels, cols=#cycles
 noc = len(Data)/512/256 # number of cycles, 512 data lines per pixel per cycle,\
     #256 pixels
 
-i=0
+
 k=0
+
 while k!=noc:
+    i=0
     while i<256:
-        Data_matrix[i][i*512:i*512+512] = Data[i*512:i*512+512]/2**31
-        i = i+1
+        Data_matrix[i][k*512:k*512+512] = Data[(i+256*k)*512:(i+256*k)*512+512]-2**31
+        i=i+1
     k=k+1
