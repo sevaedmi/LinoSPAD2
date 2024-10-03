Faster delta t computation, fixing error in fits, QOL updates

archive/tools/fancy_fit_lmfit.py
@@ -6,9 +6,9 @@ from matplotlib import pyplot as plt
 from pyarrow import feather as ft
 
 # Get the .feather file with delta t values
-path = r"D:\LinoSPAD2\Data\B7d\Ne703p\delta_ts_data"
+path = r"D:\LinoSPAD2\Data\B7d\1000\delta_ts_data"
 os.chdir(path)
-ft_file = r"0000000474-0000000523.feather"
+ft_file = r"0000007309-0000007408.feather"
 
 # Drop unnecessary values
 data = ft.read_feather(ft_file)
@@ -23,7 +23,10 @@ multiplier = number_of_bins / (2500 / 140)
 bins = np.arange(np.min(data), np.max(data), 2500 / 140 * 4)
 counts, bins = np.histogram(data, bins)
 # counts, bins = np.histogram(data, bins='scott')
+
+# Normalize
 counts = counts / np.median(counts)
+
 bin_edges = (bins[:-1] + bins[1:]) / 2
 
 # Composite fit: Gaussian for the peak + linear for bckg of random

src/LinoSPAD2/functions/data_quality.py
@@ -72,7 +72,7 @@ def sensor_population_by_cycle(
     firmware_version: str,
     timestamps: int = 512,
     app_mask: bool = True,
-    include_offset: bool = True,
+    include_offset: bool = False,
     apply_calibration: bool = True,
     absolute_timestamps: bool = False,
     correct_pixel_addressing: bool = False,
@@ -240,7 +240,7 @@ def pixel_population_by_cycle(
     motherboard_number: str,
     firmware_version: str,
     timestamps: int = 512,
-    include_offset: bool = True,
+    include_offset: bool = False,
     apply_calibration: bool = True,
     absolute_timestamps: bool = False,
     color: str = "rebeccapurple",
@@ -386,7 +386,7 @@ def get_time_ratios(
     motherboard_number: str,
     firmware_version: str,
     timestamps: int,
-    include_offset: bool = True,
+    include_offset: bool = False,
     apply_calibration: bool = True,
 ):
     """
@@ -576,6 +576,8 @@ def _extend_spread_range(spread_bins, spread_counts, extension: int):
 def sigma_of_count_spread_to_average(
     path: str,
     pixels: list,
+    range_left: float = 20e3,
+    range_right: float = 40e3,
     step: int = 10,
     bins_sigma: int = 20,
     extend: int = 0,
@@ -596,6 +598,12 @@ def sigma_of_count_spread_to_average(
         Feather file with timestamp differences.
     pixels : list
         Pixels which should be used for analysis.
+    range_left : int, optional
+        Lower limit for timestamp differences, lower values are not used.
+        The default is 20e3.
+    range_right : int, optional
+        Upper limit for timestamp differences, higher values are not used.
+        The default is 40e3.
     step : int, optional
         Multiplier of the average LinoSPAD2 TDC width of 17.857 ps that
         is used for histograms. The default is 10.
@@ -618,7 +626,7 @@ def sigma_of_count_spread_to_average(
 
     # Cut the data from the background only; without the offset
     # calibration, the delta t peak rarely goes outside the 10 ns mark
-    data_cut = data_cut[(data_cut > 20e3) & (data_cut < 40e3)]
+    data_cut = data_cut[(data_cut > range_left) & (data_cut < range_right)]
 
     # Bins in units of 17.857 ps of the average LinoSPAD2 TDC bin width
     bins = np.arange(

src/LinoSPAD2/functions/fits.py
@@ -179,11 +179,13 @@ def fit_with_gaussian(
     to_fit_n = utils.gaussian(to_fit_b, par[0], par[1], par[2], par[3])
 
     perr = np.sqrt(np.diag(pcov))
-    contrast = par[0] / par[3] * 100
+    # contrast = par[0] / par[3] * 100
+    contrast = par[0] / par[3]
     vis_er = utils.error_propagation_division(par[0], perr[0], par[3], perr[3])
 
     # Contrast error in %
-    vis_er = vis_er / (contrast / 100) * 100
+    # vis_er = vis_er / (contrast / 100) * 100
+    vis_er = vis_er
 
     fig = plt.figure(figsize=(16, 10))
     plt.locator_params(axis="x", nbins=5)
@@ -203,7 +205,8 @@ def fit_with_gaussian(
         label="fit\n"
         "\u03C3=({p1}\u00B1{pe1}) ps\n"
         "\u03BC=({p2}\u00B1{pe2}) ps\n"
-        "C=({contrast}\u00B1{vis_er}) %\n"
+        # "C=({contrast}\u00B1{vis_er}) %\n"
+        "C={contrast}\u00B1{vis_er}\n"
         "bkg={bkg}\u00B1{bkg_er}".format(
             p1=format(par[2], ".0f"),
             p2=format(par[1], ".0f"),
@@ -211,8 +214,8 @@ def fit_with_gaussian(
             pe2=format(perr[1], ".0f"),
             bkg=format(par[3], ".0f"),
             bkg_er=format(perr[3], ".0f"),
-            contrast=format(contrast, ".1f"),
-            vis_er=format(vis_er, ".1f"),
+            contrast=format(contrast, ".2f"),
+            vis_er=format(vis_er, ".2f"),
         ),
     )
     plt.legend(loc="best")

src/LinoSPAD2/functions/unpack.py
@@ -26,7 +26,7 @@ def unpack_binary_data(
     motherboard_number: str,
     firmware_version: str,
     timestamps: int = 512,
-    include_offset: bool = True,
+    include_offset: bool = False,
     apply_calibration: bool = True,
 ) -> np.ndarray:
     """Unpacks binary-encoded data from LinoSPAD2 firmware version 2212.
@@ -202,7 +202,7 @@ def unpack_binary_data_with_absolute_timestamps(
     motherboard_number: str,
     firmware_version: str,
     timestamps: int = 512,
-    include_offset: bool = True,
+    include_offset: bool = False,
     apply_calibration: bool = True,
 ):
     """Unpacks binary-encoded data from LinoSPAD2 firmware version 2212